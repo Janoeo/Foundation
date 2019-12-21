d957a4daa6f0f622599aeacee670b17091f0fc77
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2019-12-21 17:26:01 +0100

    port to 1.15.1

--- src/main/java/fr/alasdiablo/janoeo/world/EndOreGenerator.java
+++ src/main/java/fr/alasdiablo/janoeo/world/EndOreGenerator.java
@@ -4,10 +4,11 @@ import fr.alasdiablo.janoeo.config.EndConfig;
 import fr.alasdiablo.janoeo.config.GlobalConfig;
 import fr.alasdiablo.janoeo.ore.end.EndOresBlocks;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.pattern.BlockMatcher;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.ReplaceBlockConfig;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placement.CountConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,13 +20,20 @@ public class EndOreGenerator implements IWorldGenerator {
         GlobalConfig.Config globalConfig = GlobalConfig.CONFIG;
         EndConfig.Config endConfig = EndConfig.CONFIG;
         if (globalConfig.END_ORE_GEN.get()) ForgeRegistries.BIOMES.forEach(biome -> {
-            if (endConfig.COAL_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.COAL_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(68, 20, 0, 70)));
-            if (endConfig.DIAMOND_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.DIAMOND_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(20, 20, 0, 70)));
-            if (endConfig.EMERALD_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.EMERALD_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(8, 20, 0, 70)));
-            if (endConfig.GOLD_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.GOLD_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(32, 20, 0, 70)));
-            if (endConfig.IRON_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.IRON_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(40, 20, 0, 70)));
-            if (endConfig.LAPIS_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.LAPIS_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(20, 20, 0, 70)));
-            if (endConfig.REDSTONE_END_ORE.get()) biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(Feature.EMERALD_ORE, new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.REDSTONE_END_ORE.getDefaultState()), Placement.COUNT_RANGE, new CountRangeConfig(32, 20, 0, 70)));
+            if (endConfig.COAL_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.COAL_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(68, 20, 0, 70))));
+            if (endConfig.DIAMOND_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.DIAMOND_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(20, 20, 0, 70))));
+            if (endConfig.EMERALD_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.EMERALD_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(8, 20, 0, 70))));
+            if (endConfig.GOLD_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.GOLD_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(32, 20, 0, 70))));
+            if (endConfig.IRON_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.IRON_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(40, 20, 0, 70))));
+            if (endConfig.LAPIS_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.LAPIS_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(20, 20, 0, 70))));
+            if (endConfig.REDSTONE_END_ORE.get())
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.EMERALD_ORE.func_225566_b_(new ReplaceBlockConfig(Blocks.END_STONE.getDefaultState(), EndOresBlocks.REDSTONE_END_ORE.getDefaultState())).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(32, 20, 0, 70))));
         });
     }
 }
