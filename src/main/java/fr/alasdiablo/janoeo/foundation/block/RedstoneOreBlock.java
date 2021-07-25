9ffca5bd49d3dcb8b4ea1bc4ff711fd50ccd32a5
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2021-07-25 14:58:26 +0200

    fix: deepslate ore and redstone ore
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

---
+++ src/main/java/fr/alasdiablo/janoeo/foundation/block/RedstoneOreBlock.java
@@ -0,0 +1,35 @@
+package fr.alasdiablo.janoeo.foundation.block;
+
+import fr.alasdiablo.diolib.generic.ExperienceRarity;
+import fr.alasdiablo.diolib.generic.IDropExperience;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.RedStoneOreBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class RedstoneOreBlock extends RedStoneOreBlock implements IDropExperience {
+    private final ExperienceRarity experienceRarity;
+
+    public RedstoneOreBlock(Properties properties, ExperienceRarity experienceRarity, String registryName) {
+        super(properties.randomTicks().lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 9 : 0));
+        this.experienceRarity = experienceRarity;
+        this.setRegistryName(registryName);
+    }
+
+    @Override
+    public ExperienceRarity getExperienceRarity() {
+        return this.experienceRarity;
+    }
+
+    @Override
+    public int getExpDrop(BlockState state, LevelReader reader, BlockPos pos, int fortune, int silktouch) {
+        if (silktouch != 0) return 0;
+        int exp = this.getExperience(RANDOM, this);
+        if (exp == -1) return super.getExpDrop(state, reader, pos, fortune, silktouch);
+        else return exp;
+    }
+}
--- src/main/java/fr/alasdiablo/janoeo/foundation/data/loot/FoundationBlockLootTables.java
+++ src/main/java/fr/alasdiablo/janoeo/foundation/data/loot/FoundationBlockLootTables.java
@@ -33,6 +33,15 @@ public class FoundationBlockLootTables extends DioBlockLootTables {
         this.registerOreLootTable(FoundationBlocks.TINY_IRON_ORE, Items.IRON_NUGGET, UniformGenerator.between(2.0F, 6.0F));
         this.registerOreLootTable(FoundationBlocks.TINY_LAPIS_ORE, FoundationItems.LAPIS_NUGGET, UniformGenerator.between(2.0F, 6.0F));
         this.registerOreLootTable(FoundationBlocks.TINY_REDSTONE_ORE, FoundationItems.REDSTONE_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_COAL_ORE, FoundationItems.COAL_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_COPPER_ORE, FoundationItems.COPPER_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_DIAMOND_ORE, FoundationItems.DIAMOND_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_EMERALD_ORE, FoundationItems.EMERALD_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_GOLD_ORE, Items.GOLD_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_IRON_ORE, Items.IRON_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_LAPIS_ORE, FoundationItems.LAPIS_NUGGET, UniformGenerator.between(2.0F, 6.0F));
+        this.registerOreLootTable(FoundationBlocks.DEEPSLATE_TINY_REDSTONE_ORE, FoundationItems.REDSTONE_NUGGET, UniformGenerator.between(2.0F, 6.0F));
     }
 
     protected void registerOreLootTable(final Block tinyOreBlock, final Item nuggetItem, final NumberProvider valueRange) {
