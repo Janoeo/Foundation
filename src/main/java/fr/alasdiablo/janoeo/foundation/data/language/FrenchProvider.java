9df0ddb3e31f9feb6f6748cc50a1019ab6d86e13
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2021-11-26 09:49:41 +0100

    build: update diabolo lib and forge

--- src/main/java/fr/alasdiablo/janoeo/foundation/data/language/FrenchProvider.java
+++ src/main/java/fr/alasdiablo/janoeo/foundation/data/language/FrenchProvider.java
@@ -1,5 +1,6 @@
 package fr.alasdiablo.janoeo.foundation.data.language;
 
+import fr.alasdiablo.diolib.util.SimpleUnicode;
 import fr.alasdiablo.janoeo.foundation.Foundation;
 import fr.alasdiablo.janoeo.foundation.Registries;
 import net.minecraft.data.DataGenerator;
@@ -16,86 +17,86 @@ public class FrenchProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-        this.add(ALUMINIUM_DUST, "Poussière d'aluminium");
-        this.add(AMETHYST_DUST, "Poussière d'améthyste");
-        this.add(COAL_DUST, "Poussière de charbon");
-        this.add(COPPER_DUST, "Poussière de cuivre ");
-        this.add(DIAMOND_DUST, "Poussière de diamant");
-        this.add(EMERALD_DUST, "Poussière d'émeraude");
-        this.add(GOLD_DUST, "Poussière d'or");
-        this.add(IRON_DUST, "Poussière de fer");
-        this.add(LAPIS_DUST, "Poussière de lapis-lazuli");
-        this.add(LEAD_DUST, "Poussière de plomp");
-        this.add(NICKEL_DUST, "Poussière de nickel");
-        this.add(SILVER_DUST, "Poussière d'argent");
-        this.add(TIN_DUST, "Poussière d'étain");
-        this.add(URANIUM_DUST, "Poussière d'uranium");
+        this.add(ALUMINIUM_DUST, SimpleUnicode.encodeOf("Poussière d'aluminium"));
+        this.add(AMETHYST_DUST, SimpleUnicode.encodeOf("Poussière d'améthyste"));
+        this.add(COAL_DUST, SimpleUnicode.encodeOf("Poussière de charbon"));
+        this.add(COPPER_DUST, SimpleUnicode.encodeOf("Poussière de cuivre "));
+        this.add(DIAMOND_DUST, SimpleUnicode.encodeOf("Poussière de diamant"));
+        this.add(EMERALD_DUST, SimpleUnicode.encodeOf("Poussière d'émeraude"));
+        this.add(GOLD_DUST, SimpleUnicode.encodeOf("Poussière d'or"));
+        this.add(IRON_DUST, SimpleUnicode.encodeOf("Poussière de fer"));
+        this.add(LAPIS_DUST, SimpleUnicode.encodeOf("Poussière de lapis-lazuli"));
+        this.add(LEAD_DUST, SimpleUnicode.encodeOf("Poussière de plomp"));
+        this.add(NICKEL_DUST, SimpleUnicode.encodeOf("Poussière de nickel"));
+        this.add(SILVER_DUST, SimpleUnicode.encodeOf("Poussière d'argent"));
+        this.add(TIN_DUST, SimpleUnicode.encodeOf("Poussière d'étain"));
+        this.add(URANIUM_DUST, SimpleUnicode.encodeOf("Poussière d'uranium"));
 
-        this.add(ALUMINIUM_NUGGET, "Pépite d'aluminium");
-        this.add(COAL_NUGGET, "Morceau de charbon");
-        this.add(COPPER_NUGGET, "Pépite de cuivre");
-        this.add(DIAMOND_NUGGET, "Éclat de diamant");
-        this.add(EMERALD_NUGGET, "Éclat d'émeraude");
-        this.add(LAPIS_NUGGET, "Fragment de lapis-lazuli");
-        this.add(LEAD_NUGGET, "Pépite de plomp");
-        this.add(NICKEL_NUGGET, "Pépite de nickel");
-        this.add(REDSTONE_NUGGET, "Petit tas de redstone");
-        this.add(SILVER_NUGGET, "Pépite d'argent");
-        this.add(TIN_NUGGET, "Pépite d'étain");
-        this.add(URANIUM_NUGGET, "Pépite d'uranium");
+        this.add(ALUMINIUM_NUGGET, SimpleUnicode.encodeOf("Pépite d'aluminium"));
+        this.add(COAL_NUGGET, SimpleUnicode.encodeOf("Morceau de charbon"));
+        this.add(COPPER_NUGGET, SimpleUnicode.encodeOf("Pépite de cuivre"));
+        this.add(DIAMOND_NUGGET, SimpleUnicode.encodeOf("Éclat de diamant"));
+        this.add(EMERALD_NUGGET, SimpleUnicode.encodeOf("Éclat d'émeraude"));
+        this.add(LAPIS_NUGGET, SimpleUnicode.encodeOf("Fragment de lapis-lazuli"));
+        this.add(LEAD_NUGGET, SimpleUnicode.encodeOf("Pépite de plomp"));
+        this.add(NICKEL_NUGGET, SimpleUnicode.encodeOf("Pépite de nickel"));
+        this.add(REDSTONE_NUGGET, SimpleUnicode.encodeOf("Petit tas de redstone"));
+        this.add(SILVER_NUGGET, SimpleUnicode.encodeOf("Pépite d'argent"));
+        this.add(TIN_NUGGET, SimpleUnicode.encodeOf("Pépite d'étain"));
+        this.add(URANIUM_NUGGET, SimpleUnicode.encodeOf("Pépite d'uranium"));
 
-        this.add(ALUMINIUM_INGOT, "Lingot d'aluminium");
-        this.add(LEAD_INGOT, "Lingot de plomp");
-        this.add(NICKEL_INGOT, "Lingot de nickel");
-        this.add(SILVER_INGOT, "Lingot d'argent");
-        this.add(TIN_INGOT, "Lingot d'étain");
-        this.add(URANIUM_INGOT, "Lingot d'uranium");
+        this.add(ALUMINIUM_INGOT, SimpleUnicode.encodeOf("Lingot d'aluminium"));
+        this.add(LEAD_INGOT, SimpleUnicode.encodeOf("Lingot de plomp"));
+        this.add(NICKEL_INGOT, SimpleUnicode.encodeOf("Lingot de nickel"));
+        this.add(SILVER_INGOT, SimpleUnicode.encodeOf("Lingot d'argent"));
+        this.add(TIN_INGOT, SimpleUnicode.encodeOf("Lingot d'étain"));
+        this.add(URANIUM_INGOT, SimpleUnicode.encodeOf("Lingot d'uranium"));
 
-        this.add(BAUXITE_ORE, "Minerai de bauxite");
-        this.add(LEAD_ORE, "Minerai de plomb");
-        this.add(NICKEL_ORE, "Minerai de nickel");
-        this.add(SILVER_ORE, "Minerai d'argent");
-        this.add(TIN_ORE, "Minerai d'étain");
-        this.add(URANIUM_ORE, "Minerai d'uranium");
+        this.add(BAUXITE_ORE, SimpleUnicode.encodeOf("Minerai de bauxite"));
+        this.add(LEAD_ORE, SimpleUnicode.encodeOf("Minerai de plomb"));
+        this.add(NICKEL_ORE, SimpleUnicode.encodeOf("Minerai de nickel"));
+        this.add(SILVER_ORE, SimpleUnicode.encodeOf("Minerai d'argent"));
+        this.add(TIN_ORE, SimpleUnicode.encodeOf("Minerai d'étain"));
+        this.add(URANIUM_ORE, SimpleUnicode.encodeOf("Minerai d'uranium"));
 
-        this.add(DEEPSLATE_BAUXITE_ORE, "Minerai de bauxite des abîmes");
-        this.add(DEEPSLATE_LEAD_ORE, "Minerai de plomb des abîmes");
-        this.add(DEEPSLATE_NICKEL_ORE, "Minerai de nickel des abîmes");
-        this.add(DEEPSLATE_SILVER_ORE, "Minerai d'argent des abîmes");
-        this.add(DEEPSLATE_TIN_ORE, "Minerai d'étain des abîmes");
-        this.add(DEEPSLATE_URANIUM_ORE, "Minerai d'uranium des abîmes");
+        this.add(DEEPSLATE_BAUXITE_ORE, SimpleUnicode.encodeOf("Minerai de bauxite des abîmes"));
+        this.add(DEEPSLATE_LEAD_ORE, SimpleUnicode.encodeOf("Minerai de plomb des abîmes"));
+        this.add(DEEPSLATE_NICKEL_ORE, SimpleUnicode.encodeOf("Minerai de nickel des abîmes"));
+        this.add(DEEPSLATE_SILVER_ORE, SimpleUnicode.encodeOf("Minerai d'argent des abîmes"));
+        this.add(DEEPSLATE_TIN_ORE, SimpleUnicode.encodeOf("Minerai d'étain des abîmes"));
+        this.add(DEEPSLATE_URANIUM_ORE, SimpleUnicode.encodeOf("Minerai d'uranium des abîmes"));
 
-        this.add(TINY_BAUXITE_ORE, "Minuscule minerai de bauxite");
-        this.add(TINY_LEAD_ORE, "Minuscule minerai de plomb");
-        this.add(TINY_NICKEL_ORE, "Minuscule minerai de nickel");
-        this.add(TINY_SILVER_ORE, "Minuscule minerai d'argent");
-        this.add(TINY_TIN_ORE, "Minuscule minerai d'étain");
-        this.add(TINY_URANIUM_ORE, "Minuscule minerai d'uranium");
+        this.add(TINY_BAUXITE_ORE, SimpleUnicode.encodeOf("Minuscule minerai de bauxite"));
+        this.add(TINY_LEAD_ORE, SimpleUnicode.encodeOf("Minuscule minerai de plomb"));
+        this.add(TINY_NICKEL_ORE, SimpleUnicode.encodeOf("Minuscule minerai de nickel"));
+        this.add(TINY_SILVER_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'argent"));
+        this.add(TINY_TIN_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'étain"));
+        this.add(TINY_URANIUM_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'uranium"));
 
-        this.add(DEEPSLATE_TINY_BAUXITE_ORE, "Minuscule minerai de bauxite des abîmes");
-        this.add(DEEPSLATE_TINY_LEAD_ORE, "Minuscule minerai de plomb des abîmes");
-        this.add(DEEPSLATE_TINY_NICKEL_ORE, "Minuscule minerai de nickel des abîmes");
-        this.add(DEEPSLATE_TINY_SILVER_ORE, "Minuscule minerai d'argent des abîmes");
-        this.add(DEEPSLATE_TINY_TIN_ORE, "Minuscule minerai d'étain des abîmes");
-        this.add(DEEPSLATE_TINY_URANIUM_ORE, "Minuscule minerai d'uranium des abîmes");
+        this.add(DEEPSLATE_TINY_BAUXITE_ORE, SimpleUnicode.encodeOf("Minuscule minerai de bauxite des abîmes"));
+        this.add(DEEPSLATE_TINY_LEAD_ORE, SimpleUnicode.encodeOf("Minuscule minerai de plomb des abîmes"));
+        this.add(DEEPSLATE_TINY_NICKEL_ORE, SimpleUnicode.encodeOf("Minuscule minerai de nickel des abîmes"));
+        this.add(DEEPSLATE_TINY_SILVER_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'argent des abîmes"));
+        this.add(DEEPSLATE_TINY_TIN_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'étain des abîmes"));
+        this.add(DEEPSLATE_TINY_URANIUM_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'uranium des abîmes"));
 
-        this.add(TINY_COAL_ORE, "Minuscule minerai de charbon");
-        this.add(TINY_COPPER_ORE, "Minuscule minerai de cuivre");
-        this.add(TINY_DIAMOND_ORE, "Minuscule minerai de diamant");
-        this.add(TINY_EMERALD_ORE, "Minuscule minerai d'émeraude");
-        this.add(TINY_GOLD_ORE, "Minuscule minerai d'or");
-        this.add(TINY_IRON_ORE, "Minuscule minerai de fer");
-        this.add(TINY_LAPIS_ORE, "Minuscule minerai de lapis-lazuli");
-        this.add(TINY_REDSTONE_ORE, "Minuscule minerai de redstone");
+        this.add(TINY_COAL_ORE, SimpleUnicode.encodeOf("Minuscule minerai de charbon"));
+        this.add(TINY_COPPER_ORE, SimpleUnicode.encodeOf("Minuscule minerai de cuivre"));
+        this.add(TINY_DIAMOND_ORE, SimpleUnicode.encodeOf("Minuscule minerai de diamant"));
+        this.add(TINY_EMERALD_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'émeraude"));
+        this.add(TINY_GOLD_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'or"));
+        this.add(TINY_IRON_ORE, SimpleUnicode.encodeOf("Minuscule minerai de fer"));
+        this.add(TINY_LAPIS_ORE, SimpleUnicode.encodeOf("Minuscule minerai de lapis-lazuli"));
+        this.add(TINY_REDSTONE_ORE, SimpleUnicode.encodeOf("Minuscule minerai de redstone"));
 
-        this.add(DEEPSLATE_TINY_COAL_ORE, "Minuscule minerai de charbon des abîmes");
-        this.add(DEEPSLATE_TINY_COPPER_ORE, "Minuscule minerai de cuivre des abîmes");
-        this.add(DEEPSLATE_TINY_DIAMOND_ORE, "Minuscule minerai de diamant des abîmes");
-        this.add(DEEPSLATE_TINY_EMERALD_ORE, "Minuscule minerai d'émeraude des abîmes");
-        this.add(DEEPSLATE_TINY_GOLD_ORE, "Minuscule minerai d'or des abîmes");
-        this.add(DEEPSLATE_TINY_IRON_ORE, "Minuscule minerai de fer des abîmes");
-        this.add(DEEPSLATE_TINY_LAPIS_ORE, "Minuscule minerai de lapis-lazuli des abîmes");
-        this.add(DEEPSLATE_TINY_REDSTONE_ORE, "Minuscule minerai de redstone des abîmes");
+        this.add(DEEPSLATE_TINY_COAL_ORE, SimpleUnicode.encodeOf("Minuscule minerai de charbon des abîmes"));
+        this.add(DEEPSLATE_TINY_COPPER_ORE, SimpleUnicode.encodeOf("Minuscule minerai de cuivre des abîmes"));
+        this.add(DEEPSLATE_TINY_DIAMOND_ORE, SimpleUnicode.encodeOf("Minuscule minerai de diamant des abîmes"));
+        this.add(DEEPSLATE_TINY_EMERALD_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'émeraude des abîmes"));
+        this.add(DEEPSLATE_TINY_GOLD_ORE, SimpleUnicode.encodeOf("Minuscule minerai d'or des abîmes"));
+        this.add(DEEPSLATE_TINY_IRON_ORE, SimpleUnicode.encodeOf("Minuscule minerai de fer des abîmes"));
+        this.add(DEEPSLATE_TINY_LAPIS_ORE, SimpleUnicode.encodeOf("Minuscule minerai de lapis-lazuli des abîmes"));
+        this.add(DEEPSLATE_TINY_REDSTONE_ORE, SimpleUnicode.encodeOf("Minuscule minerai de redstone des abîmes"));
 
         this.add(
                 ((TranslatableComponent) Foundation.ITEMS_GROUP.getDisplayName()).getKey(),
