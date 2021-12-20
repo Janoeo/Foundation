19785abe63dd49bf499a7e71533cd8d2b464a409
Smartzyy <96419248+Smartzyy@users.noreply.github.com>
2021-12-20 13:44:10 +0100

    fix: add missing translation

--- src/main/java/fr/alasdiablo/janoeo/foundation/data/language/SpanishProvider.java
+++ src/main/java/fr/alasdiablo/janoeo/foundation/data/language/SpanishProvider.java
@@ -52,6 +52,13 @@ public class SpanishProvider extends LanguageProvider {
         this.add(TIN_INGOT, SimpleUnicode.encodeOf("Lingote de estaño"));
         this.add(URANIUM_INGOT, SimpleUnicode.encodeOf("Lingote de uranio"));
 
+        this.add(RAW_ALUMINIUM, "Aluminio en bruto");
+        this.add(RAW_LEAD, "Plomo en bruto");
+        this.add(RAW_NICKEL, "Níquel en  bruto");
+        this.add(RAW_SILVER, "Plata en bruto");
+        this.add(RAW_TIN, "Estaño en bruto");
+        this.add(RAW_URANIUM, "Uranio en bruto");
+        
         this.add(BAUXITE_ORE, SimpleUnicode.encodeOf("Mena de bauxita"));
         this.add(LEAD_ORE, SimpleUnicode.encodeOf("Mena de plomo"));
         this.add(NICKEL_ORE, SimpleUnicode.encodeOf("Mena de níquel"));
