c6d49c6ef8efcf46b0bcff4fb68c63dcbdad7e07
Smartzyy <96419248+Smartzyy@users.noreply.github.com>
2021-12-21 17:08:29 +0100

    feat: adding nether ores and scrap

--- src/main/java/fr/alasdiablo/janoeo/foundation/data/language/FrenchProvider.java
+++ src/main/java/fr/alasdiablo/janoeo/foundation/data/language/FrenchProvider.java
@@ -59,7 +59,7 @@ public class FrenchProvider extends LanguageProvider {
         this.add(RAW_TIN, SimpleUnicode.encodeOf("Étain brut"));
         this.add(RAW_URANIUM, SimpleUnicode.encodeOf("Uranium brut"));
 
-        this.add(SCRAP, "Scrap");
+        this.add(SCRAP, SimpleUnicode.encodeOf("ferraille"));
 
         this.add(BAUXITE_ORE, SimpleUnicode.encodeOf("Minerai de bauxite"));
         this.add(LEAD_ORE, SimpleUnicode.encodeOf("Minerai de plomb"));
@@ -107,20 +107,20 @@ public class FrenchProvider extends LanguageProvider {
         this.add(DEEPSLATE_TINY_LAPIS_ORE, SimpleUnicode.encodeOf("Minuscule minerai de lapis-lazuli des abîmes"));
         this.add(DEEPSLATE_TINY_REDSTONE_ORE, SimpleUnicode.encodeOf("Minuscule minerai de redstone des abîmes"));
 
-        this.add(NETHER_COAL_ORE, SimpleUnicode.encodeOf("Nether Coal Ore"));
-        this.add(NETHER_COPPER_ORE, SimpleUnicode.encodeOf("Nether Copper Ore"));
-        this.add(NETHER_DIAMOND_ORE, SimpleUnicode.encodeOf("Nether Diamond Ore"));
-        this.add(NETHER_EMERALD_ORE, SimpleUnicode.encodeOf("Nether Emerald Ore"));
-        this.add(NETHER_IRON_ORE, SimpleUnicode.encodeOf("Nether Iron Ore"));
-        this.add(NETHER_LAPIS_ORE, SimpleUnicode.encodeOf("Nether Lapis Lazuli Ore"));
-        this.add(NETHER_REDSTONE_ORE, SimpleUnicode.encodeOf("Nether Redstone Ore"));
+        this.add(NETHER_COAL_ORE, SimpleUnicode.encodeOf("Minerai de Charbon du Nether"));
+        this.add(NETHER_COPPER_ORE, SimpleUnicode.encodeOf("Minerai de cuivre du Nether"));
+        this.add(NETHER_DIAMOND_ORE, SimpleUnicode.encodeOf("Minerai de diamant du Nether"));
+        this.add(NETHER_EMERALD_ORE, SimpleUnicode.encodeOf("Minerai d'émeraude du Nether"));
+        this.add(NETHER_IRON_ORE, SimpleUnicode.encodeOf("Minerai de fer du Nether"));
+        this.add(NETHER_LAPIS_ORE, SimpleUnicode.encodeOf("Minerai de lapis-lazuli du Nether"));
+        this.add(NETHER_REDSTONE_ORE, SimpleUnicode.encodeOf("Minerai de redstone du Nether"));
 
-        this.add(NETHER_BAUXITE_ORE, SimpleUnicode.encodeOf("Nether Bauxite Ore"));
-        this.add(NETHER_LEAD_ORE, SimpleUnicode.encodeOf("Nether Lead Ore"));
-        this.add(NETHER_NICKEL_ORE, SimpleUnicode.encodeOf("Nether Nickel Ore"));
-        this.add(NETHER_SILVER_ORE, SimpleUnicode.encodeOf("Nether Silver Ore"));
-        this.add(NETHER_TIN_ORE, SimpleUnicode.encodeOf("Nether Tin Ore"));
-        this.add(NETHER_URANIUM_ORE, SimpleUnicode.encodeOf("Nether Uranium Ore"));
+        this.add(NETHER_BAUXITE_ORE, SimpleUnicode.encodeOf("Minerai de bauxite du Nether"));
+        this.add(NETHER_LEAD_ORE, SimpleUnicode.encodeOf("Minerai de plomb du Nether"));
+        this.add(NETHER_NICKEL_ORE, SimpleUnicode.encodeOf("Minerai de nickel du Nether"));
+        this.add(NETHER_SILVER_ORE, SimpleUnicode.encodeOf("Minerai d'argent du Nether"));
+        this.add(NETHER_TIN_ORE, SimpleUnicode.encodeOf("Minerai d'étain du Nether"));
+        this.add(NETHER_URANIUM_ORE, SimpleUnicode.encodeOf("Minerai d'uranium du Nether"));
 
         this.add(
                 ((TranslatableComponent) Foundation.ITEMS_GROUP.getDisplayName()).getKey(),
