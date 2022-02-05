dc4445799fe8290f7876bee3754221a260c96c30
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2022-02-05 11:59:46 +0100

    feat: tweaked ore generation
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

--- src/main/java/fr/alasdiablo/janoeo/foundation/config/FoundationConfig.java
+++ src/main/java/fr/alasdiablo/janoeo/foundation/config/FoundationConfig.java
@@ -8,30 +8,30 @@ import java.io.IOException;
 public class FoundationConfig {
 
     /* * * * * * * * * * * * * * * Ore * * *  * * * * * * * * * * */
-    public static final OreConfig BAUXITE_ORE_CONFIG = new OreConfig(Registries.BAUXITE_ORE, true).placementUniform(10, 0, 96, 6);
-    public static final OreConfig LEAD_ORE_CONFIG    = new OreConfig(Registries.LEAD_ORE, true).placementUniform(6, 0, 48, 6);
-    public static final OreConfig NICKEL_ORE_CONFIG  = new OreConfig(Registries.NICKEL_ORE, true).placementUniform(6, 0, 32, 4);
-    public static final OreConfig SILVER_ORE_CONFIG  = new OreConfig(Registries.SILVER_ORE, true).placementUniform(6, -16, 28, 4);
-    public static final OreConfig TIN_ORE_CONFIG     = new OreConfig(Registries.TIN_ORE, true).placementUniform(10, 0, 96, 6);
-    public static final OreConfig URANIUM_ORE_CONFIG = new OreConfig(Registries.URANIUM_ORE, true).placementUniform(6, -24, 28, 4);
+    public static final OreConfig BAUXITE_ORE_CONFIG = new OreConfig(Registries.BAUXITE_ORE, true).placementUniform(10, 8, 96, 6);
+    public static final OreConfig LEAD_ORE_CONFIG    = new OreConfig(Registries.LEAD_ORE, true).placementUniform(6, -16, 48, 6);
+    public static final OreConfig NICKEL_ORE_CONFIG  = new OreConfig(Registries.NICKEL_ORE, true).placementUniform(6, -24, 32, 4);
+    public static final OreConfig SILVER_ORE_CONFIG  = new OreConfig(Registries.SILVER_ORE, true).placementUniform(6, -56, 28, 6);
+    public static final OreConfig TIN_ORE_CONFIG     = new OreConfig(Registries.TIN_ORE, true).placementUniform(10, 0, 96, 8);
+    public static final OreConfig URANIUM_ORE_CONFIG = new OreConfig(Registries.URANIUM_ORE, true).placementUniform(6, -56, 28, 6);
 
     /* * * * * * * * * * * * * * * Tiny Ore * * *  * * * * * * * * * * */
     public static final OreConfig TINY_COAL_ORE_CONFIG       = new OreConfig(Registries.TINY_COAL_ORE, true).placementUniform(17, -36, 180, 20);
     public static final OreConfig TINY_COPPER_ORE_CONFIG     = new OreConfig(Registries.TINY_COPPER_ORE, true).placementUniform(10, -16, 112, 6);
-    public static final OreConfig TINY_DIAMOND_ORE_CONFIG    = new OreConfig(Registries.TINY_DIAMOND_ORE, true).placementUniform(8, -24, 16, 4);
-    public static final OreConfig TINY_EMERALD_ORE_CONFIG    = new OreConfig(Registries.TINY_EMERALD_ORE, true).placementUniform(2, 4, 31, 4);
-    public static final OreConfig TINY_GOLD_ORE_CONFIG       = new OreConfig(Registries.TINY_GOLD_ORE, true).placementUniform(9, -64, 32, 2);
+    public static final OreConfig TINY_DIAMOND_ORE_CONFIG    = new OreConfig(Registries.TINY_DIAMOND_ORE, true).placementUniform(8, -62, 16, 4);
+    public static final OreConfig TINY_EMERALD_ORE_CONFIG    = new OreConfig(Registries.TINY_EMERALD_ORE, true).placementUniform(2, 24, 31, 4);
+    public static final OreConfig TINY_GOLD_ORE_CONFIG       = new OreConfig(Registries.TINY_GOLD_ORE, true).placementUniform(9, -62, 32, 2);
     public static final OreConfig TINY_GOLD_ORE_EXTRA_CONFIG = new OreConfig(Registries.TINY_GOLD_ORE_EXTRA, true).placementUniform(9, 32, 80, 20);
     public static final OreConfig TINY_IRON_ORE_CONFIG       = new OreConfig(Registries.TINY_IRON_ORE, true).placementUniform(9, -24, 180, 20);
     public static final OreConfig TINY_LAPIS_ORE_CONFIG      = new OreConfig(Registries.TINY_LAPIS_ORE, true).placementTriangle(7, 16, 16, 4);
-    public static final OreConfig TINY_REDSTONE_ORE_CONFIG   = new OreConfig(Registries.TINY_REDSTONE_ORE, true).placementUniform(8, -16, 16, 8);
+    public static final OreConfig TINY_REDSTONE_ORE_CONFIG   = new OreConfig(Registries.TINY_REDSTONE_ORE, true).placementUniform(8, -62, 16, 8);
 
-    public static final OreConfig TINY_BAUXITE_ORE_CONFIG = new OreConfig(Registries.TINY_BAUXITE_ORE, true).placementUniform(10, 0, 96, 6);
-    public static final OreConfig TINY_LEAD_ORE_CONFIG    = new OreConfig(Registries.TINY_LEAD_ORE, true).placementUniform(6, 0, 48, 6);
-    public static final OreConfig TINY_NICKEL_ORE_CONFIG  = new OreConfig(Registries.TINY_NICKEL_ORE, true).placementUniform(6, 0, 32, 4);
-    public static final OreConfig TINY_SILVER_ORE_CONFIG  = new OreConfig(Registries.TINY_SILVER_ORE, true).placementUniform(6, -16, 28, 4);
-    public static final OreConfig TINY_TIN_ORE_CONFIG     = new OreConfig(Registries.TINY_TIN_ORE, true).placementUniform(10, 0, 96, 6);
-    public static final OreConfig TINY_URANIUM_ORE_CONFIG = new OreConfig(Registries.TINY_URANIUM_ORE, true).placementUniform(6, -24, 28, 4);
+    public static final OreConfig TINY_BAUXITE_ORE_CONFIG = new OreConfig(Registries.TINY_BAUXITE_ORE, true).placementUniform(10, 8, 96, 6);
+    public static final OreConfig TINY_LEAD_ORE_CONFIG    = new OreConfig(Registries.TINY_LEAD_ORE, true).placementUniform(6, -16, 48, 6);
+    public static final OreConfig TINY_NICKEL_ORE_CONFIG  = new OreConfig(Registries.TINY_NICKEL_ORE, true).placementUniform(6, -24, 32, 4);
+    public static final OreConfig TINY_SILVER_ORE_CONFIG  = new OreConfig(Registries.TINY_SILVER_ORE, true).placementUniform(6, -56, 28, 4);
+    public static final OreConfig TINY_TIN_ORE_CONFIG     = new OreConfig(Registries.TINY_TIN_ORE, true).placementUniform(10, 0, 96, 8);
+    public static final OreConfig TINY_URANIUM_ORE_CONFIG = new OreConfig(Registries.TINY_URANIUM_ORE, true).placementUniform(6, -56, 28, 6);
 
     /* * * * * * * * * * * * * * * Nether Ore * * *  * * * * * * * * * * */
     public static final OreConfig NETHER_COAL_ORE_CONFIG     = new OreConfig(Registries.NETHER_COAL_ORE, true).placementUniform(10, 10, 118, 10);
